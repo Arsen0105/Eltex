2120238f4c117ce220a596ccf9034ac867c1c236
Arsen0105 <118631903+Arsen0105@users.noreply.github.com>
2024-11-20 18:59:20 +0700

    Update 4.c

--- Part 1/Task 1/4.c
+++ Part 1/Task 1/4.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    unsigned int a = 0;
+    printf("Input positive integer: ");
+    scanf("%u", &a);
+    unsigned short byte = 0;
+    printf("Input third byte (0-255): ");
+    scanf("%hu", &byte);
+
+    unsigned int mask = ~(255 << 16);
+    unsigned int res = (a & mask) | (byte << 16);
+    printf("New positive int: %u\n", res);
+
+    return 0;
+}
