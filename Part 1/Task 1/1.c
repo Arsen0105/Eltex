3b5704f0c898706d22e647e4a75e84ed5f4f4d43
Arsen0105 <118631903+Arsen0105@users.noreply.github.com>
2024-11-20 17:38:55 +0700

    Update 1.c

--- Part 1/Task 1/1.c
+++ Part 1/Task 1/1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    unsigned int a = 0;
+    printf("Input positive integer: ");
+    scanf("%d", &a);
+
+    printf("bin: ");
+    size_t size = sizeof(a);
+    unsigned int mask = 1 << (size * 8 - 1);
+    while (mask) {
+        if (a & mask) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+
+        mask >>= 1;
+    }
+
+    return 0;
+}
