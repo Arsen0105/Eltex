dd2cd060d47bf64a7cf08ba09a237be902784ef4
Arsen0105 <118631903+Arsen0105@users.noreply.github.com>
2024-11-20 18:24:10 +0700

    Update 3.c

--- Part 1/Task 1/3.c
+++ Part 1/Task 1/3.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+    unsigned int a = 0;
+    printf("Input positive integer: ");
+    scanf("%u", &a);
+
+    size_t size = sizeof(a);
+    unsigned int mask = 1 << (size * 8 - 1);
+    unsigned short n = 0;
+    while (mask) {
+        if (a & mask) {
+            ++n;
+        }
+
+        mask >>= 1;
+    }
+    printf("Count \"1\" in bin: %hu", n);
+
+    return 0;
+}
